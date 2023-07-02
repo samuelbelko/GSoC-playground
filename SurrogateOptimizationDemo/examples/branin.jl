49b4274d5be2ba09fd03b6dd3487a530723b59d7
Samuel Belko <samuelbelko@protonmail.com>
2023-07-02 09:40:03 +0200

    branin example - shift function by delta

--- SurrogateOptimizationDemo/examples/branin.jl
+++ SurrogateOptimizationDemo/examples/branin.jl
@@ -14,12 +14,16 @@ using SurrogatesAbstractGPs
 using SurrogateOptimizationDemo
 
 # copied from BaysianOptimization.jl
+# shift x1, x2 by Δ, b/c inital sampling is randomly almost hitting an optimum
+Δ = 2.5
 branin(x::Vector; kwargs...) = branin(x[1], x[2]; kwargs...)
 function branin(x1, x2; a = 1, b = 5.1 / (4π^2), c = 5 / π, r = 6, s = 10, t = 1 / (8π),
                 noiselevel = 0)
+    x1 += Δ
+    x2 += Δ
     a * (x2 - b * x1^2 + c * x1 - r)^2 + s * (1 - t) * cos(x1) + s + noiselevel * randn()
 end
-minima(::typeof(branin)) = [[-π, 12.275], [π, 2.275], [9.42478, 2.475]], 0.397887
+minima(::typeof(branin)) = [[-π - Δ, 12.275 - Δ], [π - Δ, 2.275 - Δ], [9.42478 - Δ, 2.475 - Δ]], 0.397887
 mins, fmin = minima(branin)
 
 # --- GPs with hyperopt. -----
